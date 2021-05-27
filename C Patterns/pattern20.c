bfa9f9f1fb3cbb0928d50a479ae08713b64ab9b4
AnshulSahoo <70620057+AnshulSahoo@users.noreply.github.com>
2021-05-27 16:15:52 +0530

    Update pattern20.c

--- C Patterns/pattern20.c
+++ C Patterns/pattern20.c
@@ -20,7 +20,7 @@ int main()
     //loop for pattern
     for(i=n;i>=1;i--){
         for(j=1;j<=i;j++){
-            printf("%d ",i);
+            printf("%d ",j);
         }
         printf("\n");
     }
