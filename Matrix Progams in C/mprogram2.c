6a8968371e4d87416c683e5c1d561f6a72d6c5f1
AnshulSahoo <70620057+AnshulSahoo@users.noreply.github.com>
2021-06-02 08:58:19 +0530

    Update mprogram2.c

--- Matrix Progams in C/mprogram2.c
+++ Matrix Progams in C/mprogram2.c
@@ -1,86 +1,87 @@
-//WAP in C to subtarct one matrix from another
+//WAP in C to subtarct two matrix
 
 #include<stdio.h>
-#define SIZE 3
+#define SIZE 5
 
 int main()
 {
-    int i, j, row, col;
-    int A[SIZE][SIZE];
-    int B[SIZE][SIZE];
-    int subtraction[SIZE][SIZE];
+    int i, j; 
+    int row, col;
+    int firstMatrix[SIZE][SIZE];
+    int secondMatrix[SIZE][SIZE];
+    int subMatrix[SIZE][SIZE];
 
     //Enter number of rows
-    printf("Enter the Number of rows : ");
+    printf("\nEnter the Number of rows : ");
     scanf("%d",&i);
 
     //Enter number of columns
-    printf("Enter the Number of columns : ");
+    printf("\nEnter the Number of columns : ");
     scanf("%d",&j);
 
     //Enter first matrix
-    printf("Enter the Elements of first matrix of size 3 X 3 : \n");
+    printf("\nEnter the Elements of first matrix of size 3 X 3 : \n");
 
     for(row=0;row<i;row++)
     {
         for(col=0;col<j;col++)
         {
-            scanf("%d", &A[row][col]);
+            scanf("%d", &firstMatrix[row][col]);
         }
     }
 
     //Enter second matrix
-    printf("Enter the Elements of second matrix of size 3 X 3 : \n");
+    printf("\nEnter the Elements of second matrix of size 3 X 3 : \n");
 
     for(row=0;row<i;row++)
     {
         for(col=0;col<j;col++)
         {
-            scanf("%d", &B[row][col]);
+            scanf("%d", &secondMatrix[row][col]);
         }
     }
 
     //Print first matrix
-    printf("Entered first matrix of size 3 X 3 : \n");
+    printf("\nEntered first matrix of size 3 X 3 : \n");
 
     for(row=0;row<i;row++)
     {
         for(col=0;col<j;col++)
         {
-            printf("%d\t",A[row][col]);
+            printf("%d\t",firstMatrix[row][col]);
         }
         printf("\n\n");
     }
 
     //Print second matrix
-    printf("Entered second matrix of size 3 X 3 : \n");
+    printf("\nEntered second matrix of size 3 X 3 : \n");
 
     for(row=0;row<i;row++)
     {
         for(col=0;col<j;col++)
         {
-            printf("%d\t",B[row][col]);
+            printf("%d\t",secondMatrix[row][col]);
         }
         printf("\n\n");
     }
 
-    //Subtraction of matrix
+    //Addition of matrix
     for(row=0;row<i;row++)
     {
         for(col=0;col<j;col++)
         {
-            subtraction[row][col] = A[row][col] - B[row][col];
+            subMatrix[row][col] = firstMatrix[row][col] - secondMatrix[row][col];
         }
     }
 
     //Printing result
-    printf("The subtracted of matrix of size 3 X 3 : \n");
+    printf("\nThe sum of matrix of size 3 X 3 : \n");
     
     for(row=0;row<i;row++)
     {
         for(col=0;col<j;col++)
         {
-            printf("%d\t", subtraction[row][col]);
+            printf("%d\t", subMatrix[row][col]);
         }
         printf("\n\n");
     }
